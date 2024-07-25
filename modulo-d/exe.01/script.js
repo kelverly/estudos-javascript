01c32bd005099d57c4465c4fc2d253a0842a23c2
Kelverly Santos <kelverlysantos09@gmail.com>
2024-07-25 02:38:55 -0300

    new

---
+++ modulo-d/exe.01/script.js
@@ -0,0 +1,18 @@
+function carregar(){
+    let msg = document.getElementById('msg')
+    let img = document.getElementById('imagem')
+    let data = new Date()
+    let hora = data.getHours()
+    msg.innerHTML = ` Agora são ${hora} horas:`
+    if (hora >= 0 && hora < 12){
+        img.src = 'img/manha.png'
+        document.body.style.background = '#6494ed60'
+    } else if (hora >= 12 && hora <= 18){
+        img.src = 'img/tarde.png'
+        document.body.style.background = '#ed646460'
+    }else{
+        img.src = 'img/noite.png'
+        document.body.style.background = '#313131cc'
+    }
+    
+}
