8ada8c4576ccbefa4a72ff6fac10dc731c5ad223
Kelverly Santos <kelverlysantos09@gmail.com>
2024-07-27 21:55:19 -0300

    new ex

---
+++ ex01.js
@@ -0,0 +1,25 @@
+// estrutura de repetição: while
+
+let c = 1
+    while(c <= 100){
+    console.log ('tudo bem?')         
+     c++
+ }
+
+
+
+
+
+
+
+ 
+//estrutura de repetição: DO....while 
+
+let d = 1 
+do {
+    console.log('tudo bem?')
+}while(d <= 100)      
+
+
+
+ 
---
+++ node_23183b3ba16f4.tmp.js
@@ -0,0 +1,16 @@
+// let c = 1
+// while(c <= 100){
+//     // console.log ('tudo bem?')         estrutura de repetição: while
+//     c++
+// }
+
+
+
+
+let c = 1 
+do {
+    console.log('tudo bem?')
+}while(c <=100)
+
+
+
